9b5634b0dc132e43ccb0a8c10e75cdabeedc9621
Nishar <neesharansari12341@gmail.com>
2024-12-04 17:44:22 +0530

    index upldate

--- index.php
+++ index.php
@@ -3,12 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Learn Github</title>
 </head>
 <body>
     test file
 
     <!-- Edit by my side -->
      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores ipsam quam rem quidem quasi amet aut maiores blanditiis molestias at, expedita eum consectetur.
+
+     <!-- Edit 2  -->
+      this is my changes changes 
+      <form action="">
+        <input type="text">
+        <input type="email">
+        <input type="phone">
+        <input type="textarea">
+      </form>
 </body>
 </html>
